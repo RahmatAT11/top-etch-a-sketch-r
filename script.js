c885e5e9f361ede4b131c85bae8ca0d64c732d5f
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-03-23 09:19:23 +0700

    Add a grid with js to index.html

---
+++ script.js
@@ -0,0 +1,8 @@
+const sketchContainer = document.querySelector(".sketch-container");
+const grid = document.createElement("div");
+
+grid.style.backgroundColor = "blue";
+grid.style.width = "200px";
+grid.style.height = "200px";
+
+sketchContainer.appendChild(grid);
