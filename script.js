b26eb562d8d475f9c2e9dded86bde284358483d3
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-04-02 09:23:07 +0700

    Grid now created using js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const sketchContainer = document.querySelector(".sketch-container");
-const grid = document.createElement("div");
+// grid.style.width = "200px";
+// grid.style.height = "200px";
 
-grid.style.backgroundColor = "blue";
-grid.style.width = "200px";
-grid.style.height = "200px";
+const createGrid = () => {
+    for (let i = 0; i < 16*16; i++) {
+        const grid = document.createElement("div");
 
-sketchContainer.appendChild(grid);
+        grid.style.backgroundColor = "blue";
+        grid.className = `grid`
+
+        sketchContainer.appendChild(grid);
+        console.log(i)
+    }
+}
+
+createGrid();
