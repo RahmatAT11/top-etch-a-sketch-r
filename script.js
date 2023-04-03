50a3cb512a91453eb1cb9aee95351b131cd9d24a
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-04-03 11:06:37 +0700

    Grid auto-clean up when create new grid

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ let isNotMaxValue = true;
 // grid.style.height = "200px";
 
 const createGrid = (gridSize) => {
-    for (let i = 0; i < gridSize**2; i++) {
+    for (let i = 0; i < Math.pow(gridSize, 2); i++) {
         const grid = document.createElement("div");
 
-        grid.className = `grid`
+        grid.className = `grid`;
+        grid.id = `grid${i}`;
+
+        grid.onmouseenter = () => {
+            grid.style.backgroundColor = "white";
+        }
 
         sketchContainer.appendChild(grid);
     }
@@ -18,11 +23,23 @@ const createGrid = (gridSize) => {
     sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, 10px)`
 }
 
+const destroyGrid = () => {
+    if (sketchContainer.children.length > 0) {
+        const iterate = sketchContainer.children.length;
+        for (let i = 0; i < iterate; i++) {
+            const child = sketchContainer.firstChild;
+            console.log(i)
+            sketchContainer.removeChild(child);
+        }
+    }
+}
+
 const appsSetup = () => {
     do {
+        destroyGrid();
         const gridSize = parseInt(window.prompt("Please enter the grid size"));
     
-        if (gridSize <= maxValue) {
+        if (gridSize <= maxValue && gridSize > 0) {
             createGrid(gridSize);
             isNotMaxValue = false;
         }
@@ -30,5 +47,3 @@ const appsSetup = () => {
 }
 
 createGridBtn.onclick = () => appsSetup();
-
-console.log(createGridBtn)
