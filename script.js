23386fe586446e151588f9b1936e6aad6e18e7e1
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2023-04-03 09:32:06 +0700

    Input from prompt now create grid
    - input limited to 100

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const maxValue = 100;
+let isNotMaxValue = true;
 // grid.style.width = "200px";
 // grid.style.height = "200px";
 
-const createGrid = () => {
-    for (let i = 0; i < 16*16; i++) {
+const createGrid = (gridSize) => {
+    for (let i = 0; i < gridSize**2; i++) {
         const grid = document.createElement("div");
 
         grid.className = `grid`
 
         sketchContainer.appendChild(grid);
     }
+
+    sketchContainer.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
+    sketchContainer.style.gridTemplateRows = `repeat(${gridSize}, 50px)`
 }
 
-createGrid();
+do {
+    const gridSize = parseInt(window.prompt("Please enter the grid size"));
+
+    if (gridSize <= maxValue) {
+        createGrid(gridSize);
+        isNotMaxValue = false;
+    }
+} while (isNotMaxValue);
